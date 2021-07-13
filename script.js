cf6dfc45f77708232d967d0bf84fd23cb63f3784
theDaveB <davebloisesquire@gmail.com>
2021-07-12 18:13:27 -0700

    adding password length validation

--- script.js
+++ script.js
@@ -16,13 +16,26 @@ function getRequirements() {
         passLength: 0
     }
 
+    // grabbing specific requirements
     var requirementsPrompt = prompt("State your requirements, please separate with commas. (uppercase, lowercase, numbers, special)");
+    // grabbing password length
     var passLengthPrompt = prompt("How long should your password be? (Needs to be between 8 and 128 characters)");
 
-    passLengthPrompt = parseInt(passLengthPrompt);
-    passwordRequirements.passLength = passLengthPrompt;
-    requirementsPrompt = requirementsPrompt.replace(" ", "");
-    requirementsPrompt = requirementsPrompt.toLowerCase();
+    //length input validation
+    if (typeof passLength === number) {
+      passLengthPrompt = parseInt(passLengthPrompt);
+      if (passLengthPrompt >= 8 && passLengthPrompt <= 128) {
+        passwordRequirements.passLength = passLengthPrompt;
+        requirementsPrompt = requirementsPrompt.replace(" ", "");
+        requirementsPrompt = requirementsPrompt.toLowerCase();
+      } else {
+        alert("Password length needs to be between 8-128")
+      }
+
+    } else {
+      alert("Password length needs to be a number")
+    }
+
 
     var requirementsArray = requirementsPrompt.split(",");
     passwordRequirements.uppercase = requirementsArray.includes("uppercase");
